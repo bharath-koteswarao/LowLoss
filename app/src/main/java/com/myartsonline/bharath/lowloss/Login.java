9dfeed6725a3a40c21a1c31802c4985038570b76
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-04-14 20:38:02 +0530

    Second

--- app/src/main/java/com/myartsonline/bharath/lowloss/Login.java
+++ app/src/main/java/com/myartsonline/bharath/lowloss/Login.java
@@ -1,13 +1,22 @@
 package com.myartsonline.bharath.lowloss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton=(Button)findViewById(R.id.loginButton);
+    }
+
+    public void signIn(View view) {
+        loginButton.setText("SIGNING IN");
+        startActivity(new Intent(this,MainActivity.class));
     }
 }
